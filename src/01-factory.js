71c6bdb56f41bc32211e69abf69788aaf775ed83
zengchen98@163.com <zengchen98@163.com>
2019-06-04 21:01:18 +0800

    commit

--- src/01-factory.js
+++ src/01-factory.js
@@ -1,4 +1,4 @@
-class Product {
+/* class Product {
   constructor(name) {
     this.name = name;
   }
@@ -16,4 +16,22 @@ class Creator {
 // test
 let creator = new Creator();
 let p = creator.create('p1');
-p.init();
+p.init(); */
+
+class Product {
+  constructor(name) {
+    this.name = name
+  }
+  init() {
+    console.log(`${this.name} init...`)
+  }
+}
+
+class Creator {
+  create(name) {
+    return new Product(name)
+  }
+}
+
+let c = new Creator();
+c.create('xbl').init();
